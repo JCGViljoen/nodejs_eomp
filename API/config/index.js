93bbb9c7cd8a82971e3a401ef9938f9371afe6f6
JCGViljoen <jcgviljoen51@gmail.com>
2023-08-20 19:46:09 +0200

    db config

--- API/config/index.js
+++ API/config/index.js
@@ -1,1 +1,13 @@
-// config
+// db config
+require("dotenv").config();
+const{ createPool } = require("mysql");
+const connection = createPool({
+    host: process.env.dbHost,
+    database: process.env.dbName,
+    user: process.env.dbUser,
+    password: process.env.dbPwd,
+    multipleStatements: true,
+    connectionLimit: 30
+
+});
+module.exports = connection;
