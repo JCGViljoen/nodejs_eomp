12a90cea950c8be915cabea0f18c321b6ff1aa74
JCGViljoen <jcgviljoen51@gmail.com>
2023-08-20 20:49:29 +0200

    did err handeling

--- API/middelware/errorHandeling.js
+++ API/middelware/errorHandeling.js
@@ -1,1 +1,13 @@
 // err handeling
+function errorHandeling( err, req, res, next) {
+    if (err) {
+        let status = err.status || 500
+        res.json({
+            status,
+            msg: "An error occurred, try again later 😥"
+        })
+        next()
+    }
+}
+
+module.exports = errorHandeling;
