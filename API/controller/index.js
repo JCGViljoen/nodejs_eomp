297d97a730e326d46e6d2f1cc5a789ef6e7dcadc
JCGViljoen <jcgviljoen51@gmail.com>
2023-08-20 20:31:46 +0200

    added routes

--- API/controller/index.js
+++ API/controller/index.js
@@ -1,1 +1,35 @@
-// controller
+const express = require("exress");
+const bodyParser = require("body-parser");
+const routes = express.Router();
+
+// import model objects
+const { products } = require("../model");
+
+// products router
+// CREATE
+routes.post(".products", bodyParser.json(), (req,res)=> {
+    products.createProduct(req,res)
+});
+
+// READ 
+routes.get("/products", (req, res) => {
+    products.fetchProducts(req, res);
+});
+routes.get("/product/:id", (req,res) => {
+    products.fetchProduct(req,res);
+});
+
+// UPDATE
+routes.patch("/products/:id", bodyParser.json(), (req,res) => {
+    products.updateProduct(req,res);
+});
+
+// DELETE
+routes.delete("/products/:id", bodyParser.json(), (req, res) => {
+    products.deleteProduct(req, res);
+});
+
+module.exports = {
+    express,
+    routes
+};
