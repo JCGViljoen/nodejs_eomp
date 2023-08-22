4fbb10e05ad81999d855bb1ed3d203755b8d6d14
Sjdewet <summerdewet@gmail.com>
2023-08-22 11:40:14 +0200

    component

---
+++ nodejseomp/src/components/NavBar.vue
@@ -0,0 +1,63 @@
+<template>
+  <nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+      <a class="navbar-brand" id="navpic" href="#">HOMESPACE</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li>
+            <router-link class="head" to="/">Home</router-link>
+          </li>
+          <li>
+            <router-link class="head" to="/about">About Us</router-link>
+          </li>
+          <li>
+            <router-link class="head" to="/admin">Admin</router-link>
+          </li>
+          <li>
+            <router-link class="head" to="/products">Products</router-link>
+          </li>
+          <li>
+            <router-link class="head" to="/contact">Contact</router-link>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+  export default {
+  }
+</script>
+<style scoped>
+.navbar{
+  background-color: #d4a373;
+}
+.head{
+  color: white;
+  margin-inline-end: 1rem;
+  text-decoration: none;
+  padding: 11px; 
+}
+
+#navpic{
+  width:28rem;
+}
+
+.img{
+  width: 17%;
+}
+.head:hover{
+  color: rgb(0, 0, 0);
+  background-color: #a97847;
+  transition: background-color 0.3s ease;
+  border-radius: 20px;
+}
+
+</style>
--- nodejseomp/src/router/index.js
+++ nodejseomp/src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import AdminView from '../views/AdminView.vue'
+import ProductsView from '../views/ProductsView.vue'
+import ContactView from '../views/ContactView.vue'
 
 const routes = [
   {
@@ -10,11 +14,24 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: AboutView
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: AdminView
+  },
+  {
+    path: '/products',
+    name: 'products',
+    component: ProductsView
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: ContactView
   }
+
 ]
 
 const router = createRouter({
