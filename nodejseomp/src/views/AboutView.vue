85eb40b0929cfe5ce16a427eab2609aebe802a5b
Sjdewet <summerdewet@gmail.com>
2023-08-23 14:12:17 +0200

    crud testing

--- nodejseomp/src/views/AboutView.vue
+++ nodejseomp/src/views/AboutView.vue
@@ -24,6 +24,8 @@
     <div class="container">
       <h6 class="h1tag">At HomeSpace, we understand that your home is a reflection of your unique taste and style. That's why we curate a carefully selected collection of home decor items that cater to a variety of preferences, from classic and timeless designs to modern and contemporary aesthetics. Our team of experienced buyers scours the market to bring you the latest trends and innovative designs, ensuring that you have access to the best products available. We believe that your home should be a sanctuary, a place where you can relax, unwind, and express your personal style. With this in mind, we have carefully curated a collection of home decor items that cater to a variety of tastes and preferences. From kitchen and lighting to Bedroom Decor, wall art, and decorative accessories, each piece in our store is handpicked for its quality, craftsmanship, and design. At HomeSpace, we prioritize customer satisfaction above all else. We understand that every customer is unique, and we are dedicated to providing personalized attention to ensure that your shopping experience with us is exceptional</h6>
       <br>
+      <router-link to="/products" class="btn">View Products</router-link>
+      <br>
       <br>
     </div>
     <h4 class="text-center"> Watch this✅ to help you avoid Interior Design Mistakes❌</h4>
@@ -44,7 +46,11 @@
   text-align: center;
   margin-top: 1rem;
 }
-
+ .btn{
+  color: rgb(0, 0, 0);
+  background-color: #d4a373;
+  margin-left: 35rem;
+ }
 .vid{
   display: flex;
   justify-content: center;
--- nodejseomp/src/views/AdminView.vue
+++ nodejseomp/src/views/AdminView.vue
@@ -1,22 +1,46 @@
 <template>
     <div class="about">
       <div class="table-responsive" style="margin-top: 1rem">
-        <table  class="table">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Quantity</th>
-              <th>Amount</th>
-              <th>Category</th>
-              <th>Image</th>
-              <th>Edit</th>
-              <th>Delete</th>
-            </tr>
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Quantity</th>
+                <th scope="col">Amount</th>
+                <th scope="col">Category</th>
+                <th scope="col">Image</th>
+                <th scope="col">Edit</th>
+                <th scope="col">Delete</th>
+              </tr>
+            </thead>
+            <tbody v-for="item in homedecor" :key="item.prodID">
+              <tr>
+                <th scope="row">{{ item.prodID }}</th>
+                <td>{{ item.prodName }}</td>
+                <td>{{ item.quantity }}</td>
+                <td>{{ item.amount }}</td>
+                <td>{{ item.Category }}</td>
+                <td><img :src="item.prodUrl" :alt="item.prodName" style="width: 5rem"></td>
+              </tr>
 
-          </thead>
+            </tbody>
           </table>
     </div>
     </div>
   </template>
+
+  <script>
+  export default {
+    computed:{
+      homedecor(){
+        return this.$store.state.homedecor
+      }
+    },
+    mounted(){
+      this.$store.dispatch('fetchHomedecor')
+    }
+  }
+  </script>
+  
   
